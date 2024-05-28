795657fa6a3ebe0fe3c7ea9f883207cac4c6bb5a
PerryLewis01 <pl496@cam.ac.uk>
2024-05-28 15:01:02 +0100

    Plotly!!! to the rescue

--- arduinoscope.py
+++ arduinoscope.py
@@ -1,59 +1,19 @@
 #authors Nathan & Perry
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import serial
 import numpy as np
 import time as theTime
 
 from serial.tools import list_ports
 
-"""
-from serial.tools import list_ports
-port = list(list_ports.comports())
-for p in port:
-    print(p.device)
 
+import dash
+from dash import Dash, dcc, html, Input, Output, callback
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
-# Create figure for plotting
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-xs = []
-ys = []
-arduinoSerialData = serial.Serial('/dev/cu.usbmodem1401',2000000)
-# This function is called periodically from FuncAnimation
-def animate(i, xs, ys):
-    mydata = int.from_bytes(arduinoSerialData.read(2),byteorder="big", signed=False)
-    print(mydata)
-    # Add x and y to lists
-    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
-    ys.append(mydata)
-    # Limit x and y lists to 20 items
-    xs = xs[-1000:]
-    ys = ys[-1000:]
-
-    # Draw x and y lists
-    ax.clear()
-    ax.plot(xs, ys)
-    # Format plot
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-    plt.title('Data over Time')
-    plt.ylabel('Data samples')
-# Set up plot to call animate() function periodically
-anim = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=0)
-plt.show()
-
-'''
-Serial = serial.Serial('/dev/cu.usbmodem1401', 2000000)
-
-data = Serial.read(2)
-print(data.hex())
-
-'''
-"""
 
 #some data up here
-
 
 class ArduinoData:
     """
@@ -67,57 +27,6 @@ class ArduinoData:
 Livedata = np.empty(1000, dtype=object)
 for i in range (len(Livedata)): Livedata[i] = ArduinoData(0,0) 
 
-
-
-def FFT():
-    fft_vals = np.fft.fft(Livedata.voltage)
-    freq = np.fft.fftfreq(Livedata.time)
-    # Animate data
-    ax.clear()
-    ax.plot(freq, fft_vals)
-    # Format plot
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-    plt.title('Data over Frequency')
-    # Set up plot to call animate() function periodically
-    anim = animation.FuncAnimation(fig, animate, fargs=(freq, fft_vals), interval=1000)
-    plt.show()
-
-    print("FFT")
-
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-
-def animate(i, xs, ys):   
-
-    # Add x and y to lists
-    xs = [Livedata[i].time for i in range(len(Livedata))]
-    ys = [Livedata[i].voltage for i in range(len(Livedata))]
-    """# Limit x and y lists to 20 items
-    xs = xs[-1000:]
-    ys = ys[-1000:]"""
-    # Draw x and y lists
-    ax.clear()
-    ax.plot(xs, ys)
-    # Format plot
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-    plt.title('Data over Time')
-
-
-def GUI():
-    # Create figure for plotting
-
-    t = []
-    vals = []
-
-    # Set up plot to call animate() function periodically
-    anim = animation.FuncAnimation(fig, animate, fargs=(t, vals), interval=1000)
-    plt.show()
-
-
-
 def ChoosePort():
     print("\n\n\n")
     print("Please select a port from the list below\n")
@@ -130,92 +39,143 @@ def ChoosePort():
     return port[choosenPort-1].device
 
 
-prevData = bytearray([255,0,0])
-prevTime = theTime.time_ns()/10000
+def listPorts():
+    port = list(list_ports.comports())
+    portchoice = []
+    for p in port:
+        portchoice.append(p.device)
+    return portchoice
 
-def FastSerial(Arduino, prevDataint = prevData, prevTimeint = prevTime):
-    #Fast Serial communications
 
-    bytesToRead = Arduino.inWaiting()
+class aquire_data:
+    def __init__(self) -> None:
+        self.prevTime = theTime.time_ns()/1000
+        self.prevData = bytearray([255,0,0])
+        self.Arduino = serial.Serial(listPorts()[0],2000000)
 
-    if bytesToRead > 0 :
-        RawData = bytearray(Arduino.read(bytesToRead))
-        ReadTime = theTime.time_ns()/1000    # To be done as close as possible to reading the data
+    def setPort(self, choice):
+        self.Arduino = serial.Serial(choice,2000000)
 
-        
-    
-        RawData = prevDataint + RawData # joins old data to the new
-        if not(RawData[0]==255): # it must start with ff, if not shift to the next ff start
-            try:
-                RawData = RawData[RawData.index(b'\xff'):]
-            except ValueError: # when the data is corrupted return and don't process it. next buffer should work
-                return (prevDataint, prevTimeint)
-        #debug
-        print(RawData)
+    def FastSerial(self):
+        #Fast Serial communications
 
-        for i in range(round(len(RawData)/3)):
-            if RawData[(3*i)] == 255 and (RawData[(3*i)+1]>>4) == 0:
-                voltage = int.from_bytes(RawData[(3*i)+1:(3*i)+3], byteorder="big", signed=False)
-                time = prevTimeint +  i * (3 * (ReadTime - prevTimeint)/ (len(RawData)))
-                if (voltage < 4096 and voltage > 0 and (voltage - Livedata[1].voltage) < 2000):
-                    #Shift Data in
-                    Livedata[1:] = Livedata[:-1]
-                    Livedata[0] = ArduinoData(voltage, time)
-                else:
-                    #Data is corrupt ignore it. this will cause gaps
-                    pass 
-                    
-            else: #shift data until we reach the next valid point
+        bytesToRead = self.Arduino.inWaiting()
+
+        if bytesToRead > 0 :
+            RawData = bytearray(self.Arduino.read(bytesToRead))
+            ReadTime = theTime.time_ns()/1000    # To be done as close as possible to reading the data
+
+            RawData = self.prevData + RawData # joins old data to the new
+            if not(RawData[0]==255): # it must start with ff, if not shift to the next ff start
                 try:
                     RawData = RawData[RawData.index(b'\xff'):]
                 except ValueError: # when the data is corrupted return and don't process it. next buffer should work
-                    return (prevDataint, prevTimeint)
-                
+                    return 
+            #debug
+            #print(RawData)
+
+            for i in range(round(len(RawData)/3)):
+                if RawData[(3*i)] == 255 and (RawData[(3*i)+1]>>4) == 0:
+                    voltage = int.from_bytes(RawData[(3*i)+1:(3*i)+3], byteorder="big", signed=False)
+                    time = self.prevTime +  i * (3 * (ReadTime - self.prevTime)/ (len(RawData)))
+                    if (voltage < 4096 and voltage > 0 and (voltage - Livedata[1].voltage) < 2000):
+                        #Shift Data in
+                        Livedata[1:] = Livedata[:-1]
+                        Livedata[0] = ArduinoData(voltage, time)
+                    else:
+                        #Data is corrupt ignore it. this will cause gaps
+                        pass 
+                        
+                else: #shift data until we reach the next valid point
+                    try:
+                        RawData = RawData[RawData.index(b'\xff'):]
+                    except ValueError: # when the data is corrupted return and don't process it. next buffer should work
+                        pass
+                    
 
 
-        prevDataint = RawData[round(len(RawData)/3)*3:]
+            self.prevData = RawData[round(len(RawData)/3)*3:]
 
-        prevTimeint = ReadTime
+            self.prevTime = ReadTime
 
-    return (prevDataint, prevTimeint)   
+        return  
         
-        
+data_update = aquire_data()
+
+
+
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+comPorts = listPorts()
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div(
+    html.Div([
+        html.H4('ArduinoScope Live Feed'),
+        html.Div(id='live-update-text'),
+        dcc.Dropdown(comPorts, comPorts[0], id='comPortSelection'),
+        dcc.Graph(id='live-update-graph'),
+        dcc.Interval(
+            id='interval-component',
+            interval=50, # in milliseconds
+            n_intervals=0
+        )
+    ], style={'height' : '80%'})
+)
+
+"""
+@callback(Output('live-update-text', 'children'),
+              Input('interval-component', 'n_intervals'))
+def update_metrics(n):
+    lon, lat, alt = satellite.get_lonlatalt(datetime.datetime.now())
+    style = {'padding': '5px', 'fontSize': '16px'}
+    return [
+        html.Span('Longitude: {0:.2f}'.format(lon), style=style),
+        html.Span('Latitude: {0:.2f}'.format(lat), style=style),
+        html.Span('Altitude: {0:0.2f}'.format(alt), style=style)
+    ]
+"""
+
+
+@callback(Input('comPortSelection', 'value'))
+def update_com_port(comPort):
+    data_update.setPort(comPort)
+    return
+
+
+
+# Multiple components can update everytime interval gets fired.
+@callback(Output('live-update-graph', 'figure'),
+              Input('interval-component', 'n_intervals'))
+def update_graph_live(n):
+    
+    for i in range(10):
+        data_update.FastSerial()
+    fig = make_subplots(rows=2, cols=1, row_heights=[2,2])
+    fig.add_trace(
+        go.Line(x = [Livedata[i].time for i in range(len(Livedata))], y = [Livedata[i].voltage for i in range(len(Livedata))]),
+        row=1, col=1
+    )
+    
+    fig.add_trace(
+        go.Line(x = [Livedata[i].time for i in range(len(Livedata))], y = [Livedata[i].voltage for i in range(len(Livedata))]),
+        row=2, col=1
+    )
+    
+    return fig
 
 
 def plotData():
     plt.plot([Livedata[i].time for i in range(len(Livedata))], [Livedata[i].voltage for i in range(len(Livedata))], linewidth=2)
     plt.show()
 
-
-class Method():
-    def __init__(self) -> None:
-        self.myport = ChoosePort()
-        self.serialData = serial.Serial(self.myport,2000000)
-        self.prevData = bytearray(b'\xff\x00\x00')
-        self.prevTime = theTime.time_ns()/1000
-        GUI()
-        #begin GUI???
-
-
-
-    def loop(self):
-        a = FastSerial(self.serialData, prevDataint=self.prevData, prevTimeint=self.prevTime)
-        self.prevData, self.prevTime = a[0], a[1]
-        
-
-        #update GUI
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
 
-
-myProgram = Method()
-try:
-    while(1):
-        myProgram.loop()
-except KeyboardInterrupt:
-    plotData()
-    pass
 
 
 
